ef852bfac573056fc1eaf51eeb58cdb8c0f7b806
Vianey Juarez <A01797560@tec.mx>
2026-02-12 20:44:26 -0600

    Creación de proyeco A5.2

---
+++ A01797560_A5.2/computeSales.py
@@ -0,0 +1,7 @@
+
+"""
+computeSales.py
+
+Lee el catálogo de precios y un registro de ventas, calcula el costo total por venta
+y el gran total e imprime resultados en pantalla y los guarda en SalesResults.txt.
+"""
